081b007be0e13135532750baa7c3c81bd5caa66d
Corey Innis <corey@coolerator.net>
2009-11-07 03:16:53 -0500

    got the JavascriptController routing specs working (though they're a bit funky).  asset URIs may now include intermediate dots, e.g. 'javascripts/path.to/file.one.js' works

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ActionController::Routing::Routes.draw do |map|
     map.send(type, "#{type}/:path.:format", {
       :controller => "engine_assets/#{type}",
       :action     => :show,
-      :path       => /[A-Za-z0-9\/_-]+/   # e.g., path/to/file.ext
+      :path       => /[A-Za-z0-9\/._-]+?/   # e.g., path/to/file.ext
     })
   end
 end
--- spec/routing/javascripts_routing_spec.rb
+++ spec/routing/javascripts_routing_spec.rb
@@ -2,36 +2,50 @@ require 'spec_helper'
 
 describe EngineAssets::JavascriptsController do
   describe "routing" do
-    attr_reader :path
+    attr_reader :path, :route
 
     context "given a simple path" do
       before do
-        @path = '/javascripts/asset.js'
+        @path  = '/javascripts/asset.js'
+        @route = { :controller => 'engine_assets/javascripts', :action => 'show', :path => 'asset', :format => 'js' }
       end
 
-      it "is able to recognize the path" do
-        pending "my ability to overcome the challenges of spec'ing this working behavior"
-        { :get => path }.should be_routable
+      it "recognizes the path" do
+        params_from(:get, path).should == route
       end
 
-      it "is able to generate the path" do
+      it "generates the path from a URL helper" do
         javascripts_path(:asset, :format => :js).should == path
       end
     end
 
-    context "given a complex path" do
+    context "given a complex path of nested directories" do
       before do
         @path = '/javascripts/path/to/asset.js'
+        @route = { :controller => 'engine_assets/javascripts', :action => 'show', :path => 'path/to/asset', :format => 'js' }
       end
 
-      it "is able to recognize the path" do
-        pending "my ability to overcome the challenges of spec'ing this working behavior"
-        { :get => path }.should be_routable
+      it "recognizes the path" do
+        params_from(:get, path).should == route
       end
 
-      it "is able to generate the path" do
-        pending "my ability to overcome the challenges of spec'ing this working behavior"
-        javascripts_path('path/to/asset', :format => :js).should == path
+      it "generates the path from a URL helper" do
+        CGI.unescape(javascripts_path('path/to/asset', :format => :js)).should == CGI.unescape(path)
+      end
+    end
+
+    context "given a complex path of dotted components" do
+      before do
+        @path = '/javascripts/path.to/asset.one.js'
+        @route = { :controller => 'engine_assets/javascripts', :action => 'show', :path => 'path.to/asset.one', :format => 'js' }
+      end
+
+      it "recognizes the path" do
+        params_from(:get, path).should == route
+      end
+
+      it "generates the path from a URL helper" do
+        CGI.unescape(javascripts_path('path.to/asset.one', :format => :js)).should == CGI.unescape(path)
       end
     end
   end
